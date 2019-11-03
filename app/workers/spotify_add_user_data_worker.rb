83e2b9e51645680e2681636c56d58bc680921bb9
John Coote <john_coote@rocketmail.com>
2019-11-03 15:32:24 +1100

    sidekiq working locally

---
+++ app/workers/spotify_add_user_data_worker.rb
@@ -0,0 +1,62 @@
+require 'spotify_client'
+require 'pry-byebug'
+
+class SpotifyAddUserDataWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: false
+  
+  def perform(user_id, access_token)
+    @user = User.find_by(id: user_id)
+
+    @spotify_client = Spotify::Client.new(:access_token => access_token, retries: 0, raise_errors: true)
+
+    @spotify_id = @spotify_client.me["id"]
+
+    write_user_top_tracks
+  end
+
+
+   def write_user_top_tracks 
+
+    TopTrack.destroy_by(user_id: @user.id)
+
+    for timeframe in ['short_term', 'medium_term', 'long_term'] do
+      for track in top_tracks(timeframe) do
+        t = TopTrack.new
+        t.artist = track[:artist]
+        t.album = track[:album]
+        t.track = track[:track]
+        t.popularity = track[:popularity]
+        t.timeframe = timeframe
+        t.user_id = @user.id
+        t.track_id = track[:id]
+        track_features = track_audio_features(track[:id])
+        t.acousticness = track_features['acousticness']
+        t.danceability = track_features['danceability']
+        t.energy = track_features['energy']
+        t.instrumentalness = track_features['instrumentalness']
+        t.valence = track_features['valence']
+        t.tempo = track_features['tempo']
+        t.save
+      end
+    end
+  end
+
+  def top_tracks(term)
+    @spotify_client
+      .me_top_tracks(time_range: term, limit: 25)["items"]
+      .map{ |item| {
+        artist: item["album"]["artists"].first["name"],
+        album: item["album"]["name"],
+        track: item["name"],
+        popularity: item["popularity"],
+        id: item["id"]
+        }
+      }
+      .sort_by{ |hsh| hsh[:popularity] }
+  end
+
+  def track_audio_features(track_id)
+    @spotify_client.track_audio_features(track_id)
+  end
+end
