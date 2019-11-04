4b7b8e561edcf830be2eee8ff91f1b2230198106
John Coote <john_coote@rocketmail.com>
2019-11-04 21:32:35 +1100

    mods to index page - remove info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,4 @@
 class HomeController < ApplicationController
   def index
-    # all_tracks = TopTrack.all
-    @top_track = TopTrack.all
-      .map{ |t| t[:track] }
-      .group_by(&:itself)
-      .transform_values(&:count)
-      .sort_by{ |k,v| v }
-      .reverse.first.first
-
-    @top_artist = TopTrack.all
-      .map{ |t| t[:artist] }
-      .group_by(&:itself)
-      .transform_values(&:count)
-      .sort_by{ |k,v| v }
-      .reverse.first.first
-
-    @user_top_artist = current_user.top_tracks.all
-      .map{ |t| t[:artist] }
-      .group_by(&:itself)
-      .transform_values(&:count)
-      .sort_by{ |k,v| v }
-      .reverse.first.first
   end
 end
