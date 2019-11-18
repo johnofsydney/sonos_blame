502454cb5bac46cf2f08a8ed313b83c04a393b65
John Coote <john_coote@rocketmail.com>
2019-11-18 19:52:13 +1100

    more bugfixing on master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @short_termers = @user.top_tracks.where(timeframe: 'short_term')
-    @medium_termers = @user.top_tracks.where(timeframe: 'medium_term')
-    @long_termers = @user.top_tracks.where(timeframe: 'long_term')
+    short_termers = @user.top_tracks.where(timeframe: 'short_term')
+    medium_termers = @user.top_tracks.where(timeframe: 'medium_term')
+    long_termers = @user.top_tracks.where(timeframe: 'long_term')
 
-    @collections = [@short_termers, @medium_termers, @long_termers].compact
+    # @collections = [@short_termers, @medium_termers, @long_termers].compact
+
+    @collections = []
+    @collections.push(short_termers) unless short_termers.nil? || short_termers.empty?
+    @collections.push(medium_termers) unless medium_termers.nil? || medium_termers.empty?
+    @collections.push(long_termers) unless long_termers.nil? || long_termers.empty?
 
     if @user.top_artists
       @top_artists = @user.top_artists.sort_by{ |hsh| hsh[:score] }.reverse
