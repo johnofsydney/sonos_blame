af4906efd486326b8b5682d7cd70dd6da5a0dd8e
John Coote <john_coote@rocketmail.com>
2019-11-24 10:58:13 +1100

    JS working with jQuery gone global and demonstrated import of moment.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// hacked in jQuery using info from
+// http: //blog.blackninjadojo.com/ruby/rails/2019/03/01/webpack-webpacker-and-modules-oh-my-how-to-add-javascript-to-ruby-on-rails.html
+// and
+// https://stackoverflow.com/questions/55895494/is-not-defined-when-installing-jquery-in-rails-via-webpack
+// import jQuery from "jquery";
+console.log(jQuery.fn.jquery);
+window.jQuery = $;
+window.$ = $;
+
+require("packs/timexample")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/packs/timexample.js
@@ -0,0 +1,8 @@
+console.log('timexample');
+
+import moment from "moment";
+// in relation to release date of this post
+const today = moment().format("MMMM Do YYYY");
+
+console.log(today)
+
